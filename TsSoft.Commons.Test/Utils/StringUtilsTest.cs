83a79905d9f31add8d1c3805ce5c60f1b1fe6172
Victor Glushenkov <v.glushenkov@ts-soft.ru>
2013-08-23 16:58:22 +0400

    StringUtils.DefaultString

---
+++ TsSoft.Commons.Test/Utils/StringUtilsTest.cs
@@ -0,0 +1,19 @@
+﻿namespace TsSoft.Commons.Test.Utils
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TsSoft.Commons.Utils;
+
+    [TestClass]
+    public class StringUtilsTest
+    {
+        [TestMethod]
+        public void TestDefaultString()
+        {
+            Assert.AreEqual("2", StringUtils.DefaultString(null, "2"));
+            Assert.AreEqual("2", StringUtils.DefaultString("", "2"));
+            Assert.AreEqual(" ", StringUtils.DefaultString(" ", "2"));
+            Assert.AreEqual("1", StringUtils.DefaultString("1", "2"));
+        }
+    }
+}
---
+++ TsSoft.Commons/Utils/StringUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace TsSoft.Commons.Utils
+{
+    public static class StringUtils
+    {
+        public static string DefaultString(string primaryString, string defaultString)
+        {
+            return string.IsNullOrEmpty(primaryString) ? defaultString : primaryString;
+        }
+    }
+}
