4ea7d28c89bb0ab49d0dab2f81639e84a66f9510
Victor Glushenkov <v.glushenkov@ts-soft.ru>
2014-10-01 17:22:12 +0400

    AssemblyResourceHelper

---
+++ TsSoft.Commons/Utils/AssemblyResourceHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace TsSoft.Commons.Utils
+{
+    public static class AssemblyResourceHelper
+    {
+        public static Stream GetResourceStream(object namespaceProvider, string resourceName)
+        {
+            return GetResourceStream(namespaceProvider.GetType(), resourceName);
+        }
+
+        public static Stream GetResourceStream(Type namespaceProvider, string resourceName)
+        {
+            return GetResourceStream(namespaceProvider.Assembly, namespaceProvider.Namespace, resourceName);
+        }
+
+        public static Stream GetResourceStream(Assembly assembly, string resourceNamespace, string resourceName)
+        {
+            return assembly.GetManifestResourceStream(string.Format("{0}.{1}", resourceNamespace, resourceName));
+        }
+    }
+}
