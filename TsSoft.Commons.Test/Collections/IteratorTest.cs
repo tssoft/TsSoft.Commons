ac9d9db6bae3be59873a03a1ba95ba79f91f9af0
Victor Glushenkov <v.glushenkov@ts-soft.ru>
2014-10-31 13:52:22 +0300

    Iterator<T>

---
+++ TsSoft.Commons.Test/Collections/IteratorTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTestExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TsSoft.Commons.Collections;
+
+namespace TsSoft.Commons.Test.Collections
+{
+    [TestClass]
+    public class IteratorTest
+    {
+        [TestMethod]
+        public void TestHasNextEmpty()
+        {
+            var iterator = new Iterator<DayOfWeek>(null as IEnumerable<DayOfWeek>);
+            Assert.IsFalse(iterator.HasNext);
+            iterator = new Iterator<DayOfWeek>(null as IEnumerator<DayOfWeek>);
+            Assert.IsFalse(iterator.HasNext);
+            iterator = new Iterator<DayOfWeek>(Enumerable.Empty<DayOfWeek>());
+            Assert.IsFalse(iterator.HasNext);
+        }
+
+        [TestMethod]
+        public void TestHasNext()
+        {
+            var collection = Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>();
+            var iterator = new Iterator<DayOfWeek>(collection.GetEnumerator());
+            for (int i = 0; i < collection.Count(); i++)
+            {
+                Assert.IsTrue(iterator.HasNext);
+                iterator.Next();
+            }
+            Assert.IsFalse(iterator.HasNext);
+        }
+
+        [TestMethod]
+        public void TestNext()
+        {
+            IEnumerable<DayOfWeek> collection = new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Friday };
+            var iterator = new Iterator<DayOfWeek>(collection.GetEnumerator());
+            Assert.AreEqual(DayOfWeek.Monday, iterator.Next());
+            Assert.AreEqual(DayOfWeek.Friday, iterator.Next());
+            ExceptionAssert.Throws<InvalidOperationException>(() => iterator.Next());
+        }
+    }
+}
---
+++ TsSoft.Commons/Collections/Iterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TsSoft.Commons.Collections
+{
+    public interface IIterator<out T>
+    {
+        bool HasNext { get; }
+
+        T Next();
+    }
+
+    public class Iterator<T> : IIterator<T>
+    {
+        private IEnumerator<T> enumerator;
+
+        public Iterator(IEnumerable<T> enumerable)
+            : this(enumerable != null ? enumerable.GetEnumerator() : null)
+        {
+            
+        }
+
+        public Iterator(IEnumerator<T> enumerator)
+        {
+            this.enumerator = enumerator;
+            MoveNext();
+        }
+
+        public bool HasNext { get; private set; }
+
+        public T Next()
+        {
+            if (!HasNext)
+            {
+                throw new InvalidOperationException();
+            }
+            var previous = enumerator.Current;
+            MoveNext();
+            return previous;
+        }
+
+        private void MoveNext()
+        {
+            HasNext = enumerator != null && enumerator.MoveNext();
+        }
+    }
+}
