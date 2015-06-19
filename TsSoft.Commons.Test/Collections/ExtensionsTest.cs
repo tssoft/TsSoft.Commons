f4cbf9ddda0e9d83a00f0a7248af7ca94a1f1910
unknown <@ksp-w-41.test.ts-soft.ru>
2015-06-19 15:51:16 +0300

    Added Compact() method to TsSoft.Commons.Collections.Extensions

---
+++ TsSoft.Commons.Test/Collections/ExtensionsTest.cs
@@ -0,0 +1,59 @@
+﻿namespace TsSoft.Commons.Test.Collections
+{
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using TsSoft.Commons.Collections;
+
+    [TestClass]
+    public class ExtensionsTest
+    {
+        [TestMethod]
+        public void CompactGenericTest()
+        {
+            var collection = new Collection<TestClass>
+            {
+                null,
+                new TestClass(),
+                new TestClass(),
+                null,
+                new TestClass(),
+                null,
+            };
+            Assert.AreEqual(6, collection.Count);
+            Assert.AreEqual(3, collection.Count(tc => tc != null));
+            Assert.AreEqual(3, collection.Count(tc => tc == null));
+            var compactCollection = collection.Compact();
+            Assert.AreEqual(3, compactCollection.Count());
+            Assert.AreEqual(3, compactCollection.Count(tc => tc != null));
+            Assert.AreEqual(0, compactCollection.Count(tc => tc == null));
+        }
+
+        [TestMethod]
+        public void CompactStringTest()
+        {
+            const string notEmpty = "NOT_EMPTY";
+            var collection = new Collection<string>
+            {
+                null,
+                notEmpty,
+                notEmpty,
+                string.Empty,
+                "",
+                " ",
+            };
+            Assert.AreEqual(6, collection.Count);
+            Assert.AreEqual(3, collection.Count(string.IsNullOrEmpty));
+            Assert.AreEqual(3, collection.Count(tc => !string.IsNullOrEmpty(tc)));
+            var compactCollection = collection.Compact();
+            Assert.IsNotNull(compactCollection);
+            Assert.AreEqual(3, compactCollection.Count());
+            Assert.AreEqual(3, compactCollection.Count(tc => !string.IsNullOrEmpty(tc)));
+            Assert.AreEqual(0, compactCollection.Count(string.IsNullOrEmpty));
+        }
+
+        private class TestClass
+        {
+        }
+    }
+}
---
+++ TsSoft.Commons/Collections/Extensions.cs
@@ -0,0 +1,21 @@
+﻿namespace TsSoft.Commons.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> Compact<T>(this IEnumerable<T> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            return source.Where(t => t != null);
+        }
+
+        public static IEnumerable<string> Compact(this IEnumerable<string> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            return source.Where(t => !string.IsNullOrEmpty(t));
+        }
+    }
+}
